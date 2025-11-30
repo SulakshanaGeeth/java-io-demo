8cb603cf750bb969231c254c92451486ddc95515
SulakshanaGeeth <sulakshanageeth@gmail.com>
2025-11-30 13:41:50 +0530

    read from the array and print on the text

--- src/main/java/dev/geeth/Main.java
+++ src/main/java/dev/geeth/Main.java
@@ -7,10 +7,15 @@ import java.io.IOException;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
+    static void main(String[] args) {
+        String[] names = {"Product Name 01", "Product Name 02", "Product Name 02"};
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
             writer.write("Stat Writing to the File");
+            writer.write("\nHear is another line");
+            for(String name : names){
+                writer.write("\n"+name);
+            }
             writer.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
