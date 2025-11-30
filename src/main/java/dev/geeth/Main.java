242b9cc2865ec0fb3dc2cf14c3ee689a317174cb
SulakshanaGeeth <sulakshanageeth@gmail.com>
2025-11-30 13:59:46 +0530

    use BufferedReader to read files

--- src/main/java/dev/geeth/Main.java
+++ src/main/java/dev/geeth/Main.java
@@ -1,14 +1,12 @@
 package dev.geeth;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main(String[] args) {
-        String[] names = {"Product Name 01", "Product Name 02", "Product Name 02"};
+        String[] names = {"Product Name 01", "Product Name 02", "Product Name 03"};
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
             writer.write("Stat Writing to the File");
@@ -18,7 +16,20 @@ public class Main {
             }
             writer.close();
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Error occurred on Writing file : " + e.getMessage());
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("output.txt"));
+            String readLine;
+            while ((readLine = reader.readLine()) != null){
+                System.out.println(readLine);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error occurred on Reading the file : " + e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
