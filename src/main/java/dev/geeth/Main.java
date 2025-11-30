d64f74956d7fe7669440178561c4ce16a31fe92c
SulakshanaGeeth <sulakshanageeth@gmail.com>
2025-11-30 13:34:52 +0530

    write data to output.txt

--- src/main/java/dev/geeth/Main.java
+++ src/main/java/dev/geeth/Main.java
@@ -1,9 +1,19 @@
 package dev.geeth;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            writer.write("Stat Writing to the File");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
