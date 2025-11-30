2223228fc1f1e338266e73c99730b4baded46ee9
SulakshanaGeeth <sulakshanageeth@gmail.com>
2025-11-30 14:18:40 +0530

    use try with resources

--- src/main/java/dev/geeth/Main.java
+++ src/main/java/dev/geeth/Main.java
@@ -7,25 +7,23 @@ import java.io.*;
 public class Main {
     static void main(String[] args) {
         String[] names = {"Product Name 01", "Product Name 02", "Product Name 03"};
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
+            ;
             writer.write("Stat Writing to the File");
             writer.write("\nHear is another line");
             for(String name : names){
                 writer.write("\n"+name);
             }
-            writer.close();
         } catch (IOException e) {
             System.out.println("Error occurred on Writing file : " + e.getMessage());
         }
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("output.txt"));
+        try(BufferedReader reader = new BufferedReader(new FileReader("output.txt"));) {
+
             String readLine;
             while ((readLine = reader.readLine()) != null){
                 System.out.println(readLine);
             }
-            reader.close();
         } catch (FileNotFoundException e) {
             System.out.println("Error occurred on Reading the file : " + e.getMessage());
         } catch (IOException e) {
